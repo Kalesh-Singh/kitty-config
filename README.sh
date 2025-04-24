38a84f6338ec966d412463da09493e9121408bfa
Kalesh Singh <kaleshsingh96@gmail.com>
2025-04-24 00:23:38 -0700

    Initial kitty config

---
+++ README.sh
@@ -0,0 +1,9 @@
+# Kitty Config
+
+To use these configs for your kitty terminal run the following command.  
+
+NOTE: This will delete any prevous configurations you had; so make a copy of you ~/.config/kitty directory.
+
+```sh
+./config.sh
+```
---
+++ config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ~/.config/kitty
+ln -s "$PWD/kitty" ~/.config/kitty
